b77db2345eeb3f58c35e7ecf8392748e30f51495
Rythenov <376973015@qq.com>
2022-03-20 21:08:52 +0800

    1.添加SnackBar

--- app/src/main/java/com/jermyn/fluenttest2/SnackBarActivity.java
+++ app/src/main/java/com/jermyn/fluenttest2/SnackBarActivity.java
@@ -3,8 +3,10 @@ package com.jermyn.fluenttest2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.content.ContextCompat;
 import androidx.core.widget.NestedScrollView;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +16,7 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.microsoft.fluentui.appbarlayout.AppBarLayout;
+import com.microsoft.fluentui.progress.ProgressBar;
 import com.microsoft.fluentui.snackbar.Snackbar;
 
 import butterknife.BindView;
@@ -39,31 +42,35 @@ public class SnackBarActivity extends AppCompatActivity {
         setTitle("Fluent UI Snack Bar 快餐栏");
     }
 
-    @OnClick(R.id.btn_announcement_snack_bar)
-    void onAnnouncementSnackBar(){
-        Snackbar.Companion.make(rootView, "ANNOUNCEMENT Snack Bar 公告快餐栏", Snackbar.LENGTH_LONG, Snackbar.Style.ANNOUNCEMENT).show();
+    @OnClick(R.id.btn_regular_length_short_snack_bar)
+    void onRegularLengthShortSnackBar(){
+        Snackbar.Companion.make(rootView, getString(R.string.short_duration), Snackbar.LENGTH_SHORT, Snackbar.Style.REGULAR).show();
     }
 
-    @OnClick(R.id.btn_regular_snack_bar)
-    void onRegularSnackBar(){
-        Snackbar.Companion.make(rootView, "REGULAR Snack Bar 常规快餐栏", Snackbar.LENGTH_SHORT, Snackbar.Style.REGULAR).show();
+    @OnClick(R.id.btn_regular_length_short_action_snack_bar)
+    void onRegularLengthShortActionSnackBar(){
+        Snackbar.Companion.make(rootView, getString(R.string.short_duration_with_action), Snackbar.LENGTH_SHORT, Snackbar.Style.REGULAR)
+                .setAction(getString(R.string.action), v -> {
+                    Toast.makeText(SnackBarActivity.this, getString(R.string.response_action), Toast.LENGTH_SHORT).show();
+                }).show();
     }
+    @OnClick(R.id.btn_regular_length_long_custom_view_circular_progress_snack_bar)
+    void onRegularLengthLongWithCircularProgressAsSmallCustomViewSnackBar(){
+        ProgressBar circularProgress = new ProgressBar(this
+                , null
+                , 0
+                , com.microsoft.fluentui.progress.R.style.Widget_FluentUI_CircularProgress_Small);
 
-    @OnClick(R.id.btn_action_snack_bar)
-    void onActionSnackBar(){
-        Snackbar.Companion.make(rootView, "ACTION Snack Bar 动作快餐栏", Snackbar.LENGTH_SHORT, Snackbar.Style.REGULAR)
-                .setAction("ACTION", view -> {
-                    Toast.makeText(this,"触发ACTION", Toast.LENGTH_SHORT).show();
-                })
+        circularProgress.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this
+                , R.color.snackbar_circular_progress_drawable)
+                , PorterDuff.Mode.SRC_IN);
+
+        Snackbar.Companion.make(rootView, getString(R.string.long_duration_with_circular_progress_as_small_custom_view), Snackbar.LENGTH_LONG, Snackbar.Style.REGULAR)
+                .setCustomView(circularProgress, Snackbar.CustomViewSize.SMALL)
                 .show();
-
     }
 
-    @OnClick(R.id.btn_long_snack_bar)
-    void onLongSnakeBar(){
-        com.google.android.material.snackbar.Snackbar.make(rootView, "aaa", BaseTransientBottomBar.LENGTH_SHORT).show();
 
-    }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
